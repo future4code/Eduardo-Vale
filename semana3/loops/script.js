3fa7123173261d7b9772cd25198b624d9f9d348b
Eduardo do Vale <edu.dovale@gmail.com>
2020-06-04 15:08:35 -0300

    Exercício de escrita realizado (sem o desafio)

--- semana3/loops/script.js
+++ semana3/loops/script.js
@@ -11,6 +11,7 @@ console.log(sum)
 // O que o código abaixo está fazendo? 
 // Qual o resultado impresso no console? 
 // RESPOSTA: a variável soma recebe seu próprio valor mais o valor de i, parando a execução quando i for igual a 15.
+//  105
 
 */
 
@@ -40,4 +41,82 @@ console.log(novaLista)
     //           Se tivesse valor de 4, imprimiria o múltiplo de 4 presente na array lista. -> [12]
 
 */
+
+//EXERCÍCIO 3
+
+// a.  Escreva um programa que devolva o maior e o menor números contidos no array original
+
+/*
+
+const original = [12, 43, 5, 7, 31, 6732, 2999999, 6, 12, 4, 6, 224, 43, 23, 7, 323, 7, 32, 6,]
+
+let maior = 0
+
+for (const verifica of original) {
+    if (verifica > maior) {
+        maior = verifica
+    }
+}
+
+let menor = maior
+
+for (const verifica of original) {
+    if (verifica < menor) {
+        menor = verifica
+    }
+}
+
+console.log("Maior número: " + maior)
+console.log("Menor número: " + menor)
+
+*/
+
+// b.  Escreva um programa que devolva um novo array contendo todos 
+//     os valores do array original divididos por 10.
+
+/*
+
+const original = [12, 43, 5, 7, 31, 6732, 2999999, 6, 120, 4, 6, 224, 43, 23, 7, 323, 7, 32, 60,]
+let div10 = []
+
+for (verifica of original) {
+    div10.push(verifica/10)
+}
+
+console.log(div10)
+
+*/
+
+// c.  Escreva um programa que devolva um novo array contendo, somente, 
+//     os números pares do array original.
+
+/*
+
+const original = [12, 43, 5, 7, 31, 6732, 2999999, 6, 120, 4, 6, 224, 43, 23, 7, 323, 7, 32, 60,]
+let pares = []
+
+for (verifica of original) {
+    
+    if (verifica % 2 == 0) {
+        pares.push(verifica)
+    }
+}
+
+console.log(pares)
+
+*/
+
+// d.  Escreva um programa que gere um novo array contendo strings, da 
+//     seguinte forma: "O elemento do índex i é: numero"
+
+/*
+const original = [12, 43, 5, 7, 31, 6732, 2999999, 6, 120, 4, 6, 224, 43, 23, 7, 323, 7, 32, 60]
+let i = 1
+
+for (verifica of original){
+    console.log("O elemento do índex " + i + " é: " + verifica)
+    i++
+}
+*/
+
 
