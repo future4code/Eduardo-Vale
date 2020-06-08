565b4a93068d9395574d9df35ba29641f15aeebc
Eduardo do Vale <edu.dovale@gmail.com>
2020-06-08 15:27:10 -0300

    Exercícios 1, 2, 3 e 4 finalizados

---
+++ semana4/funcoes/index.js
@@ -0,0 +1,105 @@
+// function dizOi() {
+//     console.log("Vasco")
+// }
+
+// dizOi()
+
+// function somaDoisNumeros(a, b) {
+//     const soma = a + b
+//     console.log(soma)    
+// }
+
+// somaDoisNumeros(4,6)
+
+// let somaDoisNumeros = function(a, b){
+//     return a + b
+//     console.log("teste")
+// }
+
+// console.log(somaDoisNumeros(3,4))
+// const resultado = somaDoisNumeros()
+// console.log(resultado)
+
+//arrow functions
+
+// let somaDoisNumeros = (a , b) => {
+//     return a + b
+// }
+
+// const resultado = somaDoisNumeros(3,5)
+// console.log(resultado)
+
+// let imprimeArray = (array) => {
+//     for (elemento of array) {
+//         console.log = (elemento)
+
+//     }
+//     return array.length
+// }
+
+// let meuArray = [3,5,3,5,3]
+
+// const resultado = imprimeArray(meuArray)
+// console.log(resultado)
+
+// console.log("aaaaaaaaa")
+
+// EXERCÍCIO 1
+
+// 1a) []
+
+// 1b) [0, 1, 0, 1, 2 , 3]
+
+// 1c) [0, 1, 0, 1, 2, 3, 0, 1, 2, 3 ,4, 5]
+
+
+// EXERCÍCIO 2
+
+// 2a) As saídas são os índices de cada nome validados no if. 0, 2, undefined
+
+// 2b) Sim. Seria feito a comparação do índice do array com o número presente em cada posição do array. 
+// Seria impresso no console o número do index quando este for igual ao número na chamada da função.
+
+
+// EXERCÍCIO 3
+
+// Resposta: O array retorna um array de duas posições, onde o 
+// primeiro elemento é a soma de todos elementos deste novo array, 
+// e o segundo elemento é a multiplicação de todos os elementos 
+// deste mesmo array.
+//  function somaEMultiplica(array)
+
+
+// EXERCÍCIO 4
+
+// 4a)  
+
+// let idade = (a) => {
+//     return a * 7
+// }
+
+// idadeHumana = prompt("Qual a idade humana do catioro?")
+
+// resultado = idade(idadeHumana)
+// console.log("Seu catioro tem " + resultado + " anos caninos.")
+
+// 4b)
+
+// let informacao = (nome, idade, endereco, estudante) => {
+
+//     if (estudante) {
+//         estudante = "sou"
+//     } else {
+//         estudante = "não sou"
+//     }
+
+//     return `Eu sou ${nome}, tenho ${idade} anos, moro em ${endereco} e ${estudante} estudante`
+// }
+
+// let meuNome = prompt("Qual é o seu nome?")
+// let minhaIdade = prompt("Qual é a sua idade")
+// let meuEndereco = prompt("Qual é o seu endereço?")
+// let souEstudante = confirm("Se você é estudante, clique em OK")
+
+// console.log(informacao(meuNome, minhaIdade, meuEndereco, souEstudante))
+
